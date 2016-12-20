7872222aa0f3a806831ff3d80a6de86045ae8519
Grisha <aigre87@gmail.com>
2016-12-20 19:36:43 +0300

    "qwe"

--- local/templates/rudenta/inc/hpBestInRus.php
+++ local/templates/rudenta/inc/hpBestInRus.php
@@ -1,7 +1,7 @@
 <div class="hpBestInRus">
 	<div class="title">Лучшая детская стоматология России</div>
 	<div class="t">РуДента заняла 1-е место в рейтинге детских стоматологий России 2016, по версии ИД «Коммерсант» и портала SmartSmile</div>
-	<div class="itemsW">
+	<div class="itemsW iconsMenu">
 		<div class="clear items">
 			<div class="item clown">
 				<i></i>
