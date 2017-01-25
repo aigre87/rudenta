3ec0a0f205596de6ce8d78572acfa343ae4b1dc2
Viktor Burcev <Burtzev.viktor@gmail.com>
2017-01-25 19:49:19 +0400

    news count

--- recalls/index.php
+++ recalls/index.php
@@ -68,7 +68,7 @@ CModule::IncludeModule("iblock");
 				"MESSAGE_404" => "",
 				"META_DESCRIPTION" => "-",
 				"META_KEYWORDS" => "-",
-				"NEWS_COUNT" => "2",
+				"NEWS_COUNT" => "10",
 				"PAGER_BASE_LINK_ENABLE" => "N",
 				"PAGER_DESC_NUMBERING" => "N",
 				"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
