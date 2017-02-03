ee51168af7a99592e05aa431da71f070a3053c6b
Viktor Burcev <Burtzev.viktor@gmail.com>
2017-02-03 17:56:31 +0400

    фикс в деталке врача, теперь у элементов, которых нет, не выводятся заголовки

--- local/templates/rudenta/components/bitrix/news/doctors/bitrix/news.detail/.default/template.php
+++ local/templates/rudenta/components/bitrix/news/doctors/bitrix/news.detail/.default/template.php
@@ -37,8 +37,8 @@ $this->setFrameMode(true);
 	<div class="w-2col">
 		<div class="titleH3"><?php echo "Стаж работы - ", $arResult['DISPLAY_PROPERTIES']['EXPERIENCE']['NEW_VALUE'];?></div>
 		<div class="text"><?php echo $arResult['DETAIL_TEXT'];?></div>
-		<div class="titleH4">Награды и сертификаты</div>
 		<?if( $arResult['DISPLAY_PROPERTIES']['AWARDS']['VALUE'] ):?>
+			<div class="titleH4">Награды и сертификаты</div>
 			<div class="zoom-gallery clear">
 			<?foreach($arResult['DISPLAY_PROPERTIES']['AWARDS']['VALUE'] as $img):?>
 				<a class="w-1d6col" href="<?=CFile::GetPath($img);?>">
@@ -47,77 +47,40 @@ $this->setFrameMode(true);
 			<?endforeach;?>
 			</div>
 		<?endif;?>
-		<h3>Отзывы</h3>
-
-<div class="recalls-list">
-	<?foreach($arResult["ITEMS"] as $arItem):?>
-		<?
-		$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-		$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-		?>
-		<div class="recall-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-			<div class="row1">
-				<span class="name"><?=$arItem["NAME"]?></span>
-				<span class="town"><?=$arItem['DISPLAY_PROPERTIES']['CITY']['VALUE']?></span>
-			</div>
-			<?
-				$str = $arItem['DISPLAY_PROPERTIES']['RATING']['VALUE'];
-				$int = filter_var($str, FILTER_SANITIZE_NUMBER_INT);
-				$perc = intval($int)*100/5;
-			?>
-			<div class="raiting" data-val="<?=$int?>">
-				<div class="val" style="width:<?=$perc?>%;"></div>
-				<div class="bg"></div>
-			</div>
-			<div class="row2">
-				<?echo FormatDate("d F Y", MakeTimeStamp($arItem["DISPLAY_ACTIVE_FROM"]));?>
-				<?if(!empty($arItem['DISPLAY_PROPERTIES']['SERVICE']['VALUE'])):?>
-					про <?=$arItem['DISPLAY_PROPERTIES']['SERVICE']['DISPLAY_VALUE']?>
-				<?endif;?>
-				<?if(!empty($arItem['DISPLAY_PROPERTIES']['DOCTOR']['VALUE'])):?>
-					врачу <?=$arItem['DISPLAY_PROPERTIES']['DOCTOR']['DISPLAY_VALUE']?>
-				<?endif;?>
-			</div>
-			<div class="text">
-				<?echo $arItem["PREVIEW_TEXT"];?>
-			</div>
-		</div>
-	<?endforeach;?>	
-</div>
-
-
-
-		<div class="recalls-list">
-			<?foreach($arResult['RECALLS'] as $rec):?>
-				<div class="recall-item">
-					<div class="row1">
-						<span class="name"><?php echo $rec['NAME'];?></span>
-						<span class="town"><?php echo $rec['PROPERTY_CITY_VALUE'];?></span>
+		<?if(!empty($arResult['RECALLS'])):?>
+			<h3>Отзывы</h3>
+			<div class="recalls-list">
+				<?foreach($arResult['RECALLS'] as $rec):?>
+					<div class="recall-item">
+						<div class="row1">
+							<span class="name"><?php echo $rec['NAME'];?></span>
+							<span class="town"><?php echo $rec['PROPERTY_CITY_VALUE'];?></span>
+						</div>
+						<?
+							$str = $rec['PROPERTY_RATING_VALUE'];
+							$int = filter_var($str, FILTER_SANITIZE_NUMBER_INT);
+							$perc = intval($int)*100/5;
+						?>
+						<div class="raiting" data-val="<?=$int?>">
+							<div class="val" style="width:<?=$perc?>%;"></div>
+							<div class="bg"></div>
+						</div>
+						<div class="row2">
+							<?php $res = CIBlockSection::GetByID($rec['PROPERTY_SERVICE_VALUE']);?>
+							<?if($ar_res = $res->GetNext()):?>
+								про
+								<a href="/services/detail/?SECTION_ID=<?php echo $ar_res['IBLOCK_SECTION_ID'];?>">
+									<?php echo $ar_res['NAME'];?>
+								</a>
+							<?endif;?>
+						</div>
+						<div class="text">
+							<?php echo $rec['PREVIEW_TEXT'];?>
+						</div>
 					</div>
-					<?
-						$str = $rec['PROPERTY_RATING_VALUE'];
-						$int = filter_var($str, FILTER_SANITIZE_NUMBER_INT);
-						$perc = intval($int)*100/5;
-					?>
-					<div class="raiting" data-val="<?=$int?>">
-						<div class="val" style="width:<?=$perc?>%;"></div>
-						<div class="bg"></div>
-					</div>
-					<div class="row2">
-						<?php $res = CIBlockSection::GetByID($rec['PROPERTY_SERVICE_VALUE']);?>
-						<?if($ar_res = $res->GetNext()):?>
-							про
-							<a href="/services/detail/?SECTION_ID=<?php echo $ar_res['IBLOCK_SECTION_ID'];?>">
-								<?php echo $ar_res['NAME'];?>
-							</a>
-						<?endif;?>
-					</div>
-					<div class="text">
-						<?php echo $rec['PREVIEW_TEXT'];?>
-					</div>
-				</div>
-			<?endforeach;?>
-		</div>
+				<?endforeach;?>
+			</div>
+		<?endif;?>
 	</div>
 	<div class="w-1col">
 		<?$APPLICATION->IncludeComponent(
@@ -133,11 +96,13 @@ $this->setFrameMode(true);
 			false
 		);?>
 		<div class="docNoteBlock">
-			<div class="title">Заметки врача</div>
 			<?php $articles = Doctors::getArticles($arResult['ID']);?>
-			<?foreach($articles as $article):?>
-				<a href="/articles/<?=$article['ID']?>/"><?=$article['NAME']?></a>
-			<?endforeach;?>
+			<?if(!empty($articles)):?>
+				<div class="title">Заметки врача</div>
+				<?foreach($articles as $article):?>
+					<a href="/articles/<?=$article['ID']?>/"><?=$article['NAME']?></a>
+				<?endforeach;?>
+			<?endif;?>
 		</div>
 	</div>
 </div>
