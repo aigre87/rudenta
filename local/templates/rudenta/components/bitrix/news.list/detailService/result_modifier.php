45309b00b51e7b4b19b0d8040f9e640663830935
Viktor Burcev <Burtzev.viktor@gmail.com>
2017-02-03 19:02:09 +0400

    bug fix sort services

--- local/templates/rudenta/components/bitrix/news.list/detailService/result_modifier.php
+++ local/templates/rudenta/components/bitrix/news.list/detailService/result_modifier.php
@@ -1,11 +1,38 @@
 <?php
 $tmp = array();
+$tmp_for_sort = array();
 $nav = array();
+// сортировка по разделам
 foreach($arResult['ITEMS'] as $arItem){
-    $tmp[$arItem['IBLOCK_SECTION_ID']][] = $arItem;
+    $db_list = CIBlockSection::GetList(
+        array(),
+        array('IBLOCK_ID' => 4, 'ID' => $arItem['IBLOCK_SECTION_ID']),
+        false,
+        array(
+            'ID',
+            'SORT'
+        )
+    );
+
+    while($ar_result = $db_list->GetNext())
+    {
+        if($ar_result['ID'] == $arItem['IBLOCK_SECTION_ID']){
+            $tmp_for_sort[$ar_result['SORT']][] = $arItem;
+        }
+    }
 }
+
+ksort($tmp_for_sort);
+foreach($tmp_for_sort as $sort => $arItem){
+    foreach($arItem as $item){
+        $tmp[$item['IBLOCK_SECTION_ID']][] = $item;
+    }
+}
+
+
 $arResult['ITEMS'] = $tmp;
 
+// боковая навигация
 foreach($arResult['ITEMS'] as $arItemKey => $arItemArr){
     $res = CIBlockSection::GetByID($arItemKey);
     if($ar_res = $res->GetNext()){
