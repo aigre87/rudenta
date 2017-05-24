a67d17c3c355f20cb74560bc1414706522ce3cfc
Viktor Burcev <Burtzev.viktor@gmail.com>
2017-05-24 14:25:39 +0400

    404 page

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Страница не найдена");
+?>
+
+<p>Проверьте правильно ли введен адрес</p>
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
+?>
