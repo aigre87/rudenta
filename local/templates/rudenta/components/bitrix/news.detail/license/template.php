a3fe04c893b78454d47cd97f60a04d8a65f27b1a
Viktor Burcev <Burtzev.viktor@gmail.com>
2017-07-03 18:39:18 +0400

    new detail news

--- local/templates/rudenta/components/bitrix/news.detail/license/template.php
+++ local/templates/rudenta/components/bitrix/news.detail/license/template.php
@@ -12,9 +12,9 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<a class="license">
+<a class="license" download="license.pdf" href="<?=$arResult['FILE_SRC']?>">
 	<svg class="icon">
-		<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="<?=$arResult['FILE_SRC']?>"></use>
+		<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/local/templates/rudenta/images/symbol/sprite.svg#icon-gramota"></use>
 	</svg>
 	<span class="text"><?=$arResult['NAME']?></span>
 	<span class="desc">PDF &bull; <?=$arResult['FILE_SIZE']?> Кб</span>
