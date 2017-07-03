6e1b1f40b4cbb21fee5527592921429e2206a438
aigre123 <aigre87@gmail.com>
2017-07-03 13:35:08 +0300

    "front up"

--- local/templates/rudenta/header.php
+++ local/templates/rudenta/header.php
@@ -40,7 +40,14 @@ else
 
     <meta name="keywords" content="<?$APPLICATION->ShowProperty("keywords");?>" />
     <meta name="description" content="<?$APPLICATION->ShowProperty("description");?>" />
-    <meta id="vp" name="viewport" content="width=1240;  maximum-scale=1.0; user-scalable=no">
+    <meta id="vp" name="viewport" content="width=1240">
+
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+		<link rel="manifest" href="/manifest.json">
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+		<meta name="theme-color" content="#ffffff">
 
 		<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
 
