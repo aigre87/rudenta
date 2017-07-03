0dd3d55e09f5f6925fdf5adbc7563c24981b31a9
Viktor Burcev <Burtzev.viktor@gmail.com>
2017-07-03 12:20:01 +0400

    text fix

--- insurance/index.php
+++ insurance/index.php
@@ -31,7 +31,7 @@ $APPLICATION->SetTitle("Лечение по страховке");
                     <svg class="icon">
                         <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/local/templates/rudenta/images/symbol/sprite.svg#icon-print"></use>
                     </svg>
-                    Версия дял печати
+                    Версия для печати
                 </div>
                 <a class="printButton">Распечатать</a>
                 <a class="disablePrintVersion">Обычный просмотр</a>
