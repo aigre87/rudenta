4e22ae42831fb7403b1819f85eed397e19b671cf
Viktor Burcev <Burtzev.viktor@gmail.com>
2017-07-12 19:31:21 +0400

    вакансии News.list

--- local/templates/rudenta/components/bitrix/news.list/vacancy/template.php
+++ local/templates/rudenta/components/bitrix/news.list/vacancy/template.php
@@ -13,9 +13,6 @@
 $this->setFrameMode(true);
 ?>
 <div class="news-list">
-<?if($arParams["DISPLAY_TOP_PAGER"]):?>
-	<?=$arResult["NAV_STRING"]?><br />
-<?endif;?>
 <?foreach($arResult["ITEMS"] as $arItem):?>
 	<?
 	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
@@ -25,88 +22,35 @@ $this->setFrameMode(true);
 		<div class="header">
 			<?echo $arItem["NAME"]?>
 		</div>
+		<?if($arItem["PREVIEW_TEXT"]):?>
+			<div class="detailText"><?echo $arItem["PREVIEW_TEXT"];?></div>
+		<?endif;?>
 		<div class="content">
-			<?foreach($arItem["FIELDS"] as $code=>$value):?>
-				<?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?>
-			<?endforeach;?>
-			<?foreach($arItem["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
-				<div class="sectionName"><?=$arProperty["NAME"]?> :</div>
-				<?if(is_array($arProperty["DISPLAY_VALUE"])):?>
-					<div class="items"><?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?></div>
-				<?else:?>
-					<div class="items"><?=$arProperty["DISPLAY_VALUE"];?></div>
-				<?endif?>
-			<?endforeach;?>
-			<?if($arItem["DETAIL_TEXT"]):?>
-				<div class="detailText"><?echo $arItem["DETAIL_TEXT"];?></div>
-			<?endif;?>
+			<div class="duties">
+			Обязанности:
+			 <ul>
+				 <?foreach($arItem['PROPERTIES']['DUTIES']['VALUE'] as $duties_name):?>
+					<li><?=$duties_name?></li>
+				 <?endforeach;?>
+			 </ul>
+			</div>
+			<div class="requir">
+				Требования:
+				<ul>
+					<?foreach($arItem['PROPERTIES']['REQUIR']['VALUE'] as $requir_name):?>
+						<li><?=$requir_name?></li>
+					<?endforeach;?>
+				</ul>
+			</div>
+			<div class="conditions">
+				Условия:
+				<ul>
+					<?foreach($arItem['PROPERTIES']['CONDITIONS']['VALUE'] as $condition_name):?>
+						<li><?=$condition_name?></li>
+					<?endforeach;?>
+				</ul>
+			</div>
 		</div>
 	</div>
-
-	<?/*<p class="news-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-				<a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><img
-						class="preview_picture"
-						border="0"
-						src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-						width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
-						height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
-						alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-						title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-						style="float:left"
-						/></a>
-			<?else:?>
-				<img
-					class="preview_picture"
-					border="0"
-					src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-					width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
-					height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
-					alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-					title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-					style="float:left"
-					/>
-			<?endif;?>
-		<?endif?>
-		<?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
-			<span class="news-date-time"><?echo $arItem["DISPLAY_ACTIVE_FROM"]?></span>
-		<?endif?>
-		<?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
-			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-				<a href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><b><?echo $arItem["NAME"]?></b></a><br />
-			<?else:?>
-				<b><?echo $arItem["NAME"]?></b><br />
-			<?endif;?>
-		<?endif;?>
-		<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
-			<?echo $arItem["PREVIEW_TEXT"];?>
-		<?endif;?>
-		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-			<div style="clear:both"></div>
-		<?endif?>
-		<?foreach($arItem["FIELDS"] as $code=>$value):?>
-			<small>
-			<?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?>
-			</small><br />
-		<?endforeach;?>
-		<?foreach($arItem["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
-			<small>
-			<?=$arProperty["NAME"]?>:&nbsp;
-			<?if(is_array($arProperty["DISPLAY_VALUE"])):?>
-				<?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?>
-			<?else:?>
-				<?=$arProperty["DISPLAY_VALUE"];?>
-			<?endif?>
-			</small><br />
-		<?endforeach;?>
-	</p>*/?>
-
-
-
-	
 <?endforeach;?>
-<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
-	<br /><?=$arResult["NAV_STRING"]?>
-<?endif;?>
 </div>
