ffe0e23b5f3b22f01b6d9a14faca3c7701a07294
Viktor Burcev <Burtzev.viktor@gmail.com>
2017-07-14 16:03:58 +0400

    ресайз врачей в деталке

--- local/templates/rudenta/components/bitrix/news/doctors/bitrix/news.detail/.default/result_modifier.php
+++ local/templates/rudenta/components/bitrix/news/doctors/bitrix/news.detail/.default/result_modifier.php
@@ -1,3 +1,14 @@
 <?php
+
+$resizeImg = CFile::GetFileArray($arResult['DETAIL_PICTURE']['ID']);
+$resizeImg = CFile::ResizeImageGet(
+    $resizeImg,
+    array('width'=> 380, 'height'=>340),
+    BX_RESIZE_IMAGE_PROPORTIONAL,
+    true
+);
+$resizeImg['SRC'] = $resizeImg['src'];
+$arResult['DETAIL_PICTURE'] = $resizeImg;
+
 $arResult['DISPLAY_PROPERTIES']['EXPERIENCE']['NEW_VALUE'] = Doctors::getExp($arResult['PROPERTIES']['EXPERIENCE']['VALUE']);
 $arResult['RECALLS'] = Doctors::getRecalls($arResult['ID']);
