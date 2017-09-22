6d810670b6b6d92e1c93f31496e83227115751b2
aigre123 <aigre87@gmail.com>
2017-09-22 12:08:33 +0300

    "up"

--- local/templates/rudenta/header.php
+++ local/templates/rudenta/header.php
@@ -54,7 +54,7 @@ else
 
 		<link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH?>/fonts/fonts.css?v2">
 		<link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH?>/css/template_styles.min.css?v3">
-		<script type="text/javascript" async src="http://api-maps.yandex.ru/2.1/?lang=ru_RU"></script>
+		<script type="text/javascript" src="http://api-maps.yandex.ru/2.1/?lang=ru_RU"></script>
     <script type="text/javascript" src="<?=SITE_TEMPLATE_PATH?>/libs/libs.min.js?v1"></script>
     <script type="text/javascript" async src="<?=SITE_TEMPLATE_PATH?>/js/script.js?v2"></script>
     <script type="text/javascript" async src="<?=SITE_TEMPLATE_PATH?>/js/main.min.js?v1"></script>
